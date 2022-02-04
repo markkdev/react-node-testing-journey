d8efa53b510124587e2fa8915d1034ddb7afd8fc
Mark Khuzam <mkhuzam@opentable.com>
2022-02-03 22:36:12 -0800

    removed component internal files

--- src/components/ReviewFormConfirmation/index.js
+++
@@ -1,2 +0,0 @@
-export { default as ReviewFormConfirmation } from './ReviewFormConfirmation';
-export { default as ReviewFormFieldValue } from './ReviewFormFieldValue';
--- src/pages/api/submit-review.js
+++ src/pages/api/submit-review.js
@@ -1,14 +1,5 @@
 import { stringReverse } from '../../server/utils';
 
-const wait = time =>
-  new Promise((res, rej) => {
-    setTimeout(() => res(), time);
-  });
-
 export default async function handler(req, res) {
-  const { body } = req;
-  await wait(1000);
-  res
-    .status(200)
-    .json({ message: `Thank you ${stringReverse(body.firstName)}` });
+  res.status(200).json({ message: `Received` });
 }
